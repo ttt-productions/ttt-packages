6026c75958b5141674625bda9a97b22e3bee33b0
pignuggetz <pignuggetz@ttt.productions>
2026-01-14 01:05:32 +0000

    updates

---
+++ packages/ui-core/src/components/skeleton.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import * as React from "react"
+import { cn } from "../lib/utils"
+
+function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-muted", className)}
+      data-ai-hint="loading placeholder"
+      {...props}
+    />
+  )
+}
+
+export { Skeleton }
--- packages/ui-core/src/index.ts
+++ packages/ui-core/src/index.ts
@@ -1,5 +1,9 @@
 // Export all components
-export * from './components/button';
+export * from "./components/button"
+export * from "./components/card"
+export * from "./components/label"
+export * from "./components/textarea"
+export * from "./components/skeleton"
 
 // Export utilities
-export * from './lib/utils';
+export * from "./lib/utils"
